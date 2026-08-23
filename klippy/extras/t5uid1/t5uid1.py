b3d6b77160a19fb4c3274be6dcedf671e82b8296
Evan <evan@localhost>
2026-08-23 01:08:10 -0400

    Fix T5UID1 serial response registration

--- klippy/extras/t5uid1/t5uid1.py
+++ klippy/extras/t5uid1/t5uid1.py
@@ -346,8 +346,8 @@ class T5UID1:
         self._t5uid1_write_cmd = self.mcu.lookup_command(
             "t5uid1_write oid=%c command=%c data=%*s", cq=cmd_queue)
 
-        self.mcu.register_response(self._handle_t5uid1_received,
-                                   "t5uid1_received")
+        self.mcu.register_serial_response(self._handle_t5uid1_received,
+                                   "t5uid1_received command=%c data=%*s")
 
     def _handle_ready(self):
         self.toolhead = self.printer.lookup_object('toolhead')
